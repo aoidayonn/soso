49c3940294b333795c9749942431ec273598db1f
a-nishi <a-nishi@example.com>
2026-06-11 10:54:30 +0900

    TOPぺーじひょうじかんりょう！

--- soso/views.py
+++ soso/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views import generic
+from django.views.generic import View
+#from message.models import Article
+#from message.forms import ArticleCreateForm
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "soso/top.html")
+
+    def post(self, request, *args, **kwargs):
+        pass
